ed1d436e137c51924442040cae794125dd32b725
Joseph Steel <joseph.steel.iv@gmail.com>
2020-06-28 15:10:40 -0400

    minor updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Book = require('../models').Book;
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.redirect("/books");
-});
+/* GET books page and all books */
+router.get('/', asyncHandler(async (req, res) => {
+    const books = await Books.findAll({ order: [[ "createdAt", "DESC" ]]});
+    res.render("articles/index", { books: books, title: "SQL Library" });
+}));
 
 module.exports = router;
